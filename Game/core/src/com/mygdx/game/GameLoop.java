9ce1818e71a84e3468044667b30336baca4d6b68
Longphihoang <Long.p.hoang1@gmail.com>
2017-02-01 22:41:26 -0800

    Add files via upload
    Finished Timing and outOfBoundsError

--- Game/core/src/com/mygdx/game/GameLoop.java
+++ Game/core/src/com/mygdx/game/GameLoop.java
@@ -6,56 +6,71 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.utils.Timer.Task;
 
 import server.*;
 
 public class GameLoop extends ApplicationAdapter implements InputProcessor    {
 	SpriteBatch batch;
+	//Sound sound;
 	Texture img;
+	Texture img2;
+	Texture background;
 	server.Logic logic;
     float dt;
+    BitmapFont font;
     OrthographicCamera camera;
 	@Override
 	public void create () {
+		//sound = new Sound("");
 		camera= new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		//camera.position.set(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2,0);
-		
 		batch = new SpriteBatch();
 		batch.getProjectionMatrix().setToOrtho(0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0, 0, 1);
-
+		background = new Texture("background.png");
 		img = new Texture("wood.jpg");
+		img2 = new Texture("woodRotate.png");
 		logic = new Logic();
 		dt = Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f);
 		Gdx.input.setInputProcessor(this);
-	}
+		font = new BitmapFont();
+		
+		StartTick(2f,.5f);
+		
+			}
 
 	@Override
 	public void render () {
 		camera.update();
-		try {
-			TimeUnit.MILLISECONDS.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		logic.run();
+		
+		//logic.run();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		for(int i=0;i<4;i++)
+		batch.draw(background,0,0);
+		batch.draw(img2,(float) logic.p1getCurrent().getPoints(0).getX()*25,(float) logic.p1getCurrent().getPoints(0).getY()*25);
+		for(int i=1;i<4;i++)
 		{
 		batch.draw(img,(float) logic.p1getCurrent().getPoints(i).getX()*25,(float) logic.p1getCurrent().getPoints(i).getY()*25);
 		}
 		for(int i=0;i<logic.getPlayerLanded().size();i++)
 		{
 			batch.draw(img,(float) logic.getPlayerLanded().get(i).getX()*25,(float) logic.getPlayerLanded().get(i).getY()*25);
+		}
+		for(int i = 0;i<15;i++)
+		{
+			font.draw(batch, ""+i, i*25, 580);
+			font.draw(batch, ""+i, i*25, 0);
 		}
 		batch.end();
 	}
@@ -71,7 +86,23 @@ public class GameLoop extends ApplicationAdapter implements InputProcessor    {
 		// TODO Auto-generated method stub
 		if(keycode==Keys.LEFT)
 		{
-			System.out.println("WORKING");
+			logic.moveP1("left");
+			return true;
+		}
+		if(keycode==Keys.RIGHT)
+		{
+			logic.moveP1("right");
+			return true;
+		}
+		if(keycode==Keys.DOWN)
+		{
+			logic.moveP1("down");
+			return true;
+		}
+		if(keycode==Keys.UP)
+		{
+			logic.moveP1("up");
+			return true;
 		}
 		return false;
 	}
@@ -116,5 +147,19 @@ public class GameLoop extends ApplicationAdapter implements InputProcessor    {
 	public boolean scrolled(int amount) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public void StartTick(float delay, float inteval)
+	{
+		Timer.schedule(new Task(){
+            @Override
+            public void run() {
+                logic.run();
+            }
+        }
+        , delay        //    (delay)
+        , inteval     //    (seconds)
+    );
+
 	}
 }
--- Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.mygdx.game.GameLoop;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title="Fortress";
+		config.useGL30=true;
+		config.height=640;
+		config.width=960;
 		new LwjglApplication(new GameLoop(), config);
 	}
 }
